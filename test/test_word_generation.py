a9077b88ae9951f0cf8dfbc9d38158f779139ea2
[Gemma-Rate] <thebrickmaster@theratefamily.co.uk>
2018-10-27 19:09:51 +0100

    Now with tokenisation of words

--- test/test_word_generation.py
+++ test/test_word_generation.py
@@ -12,14 +12,14 @@ class TestGeneration(un.TestCase):
     def test_word_generate(self):
         """Test word generation with loaded file"""
         w = wn.get_word_list('no_stop_g2.txt')
-        word = wn.target_word(w)
+        word = wn.TargetWord(w)
         word.word_gen()
         print(word.word)
 
     def test_range_generate(self):
         """Test range generation"""
         w = wn.get_word_list('no_stop_g2.txt')
-        word = wn.target_word(w)
+        word = wn.TargetWord(w)
         word.word_gen()
         word.range_gen()
         print(word.range)
